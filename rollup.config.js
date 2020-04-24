9e071e456d067a8c9784988696e044ee9e0ff8be
Marco Starker <marco.starker@gmx.net>
2020-04-24 12:00:52 +0200

    dep update

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve';
-import progress from 'rollup-plugin-progress';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
 import clear from "rollup-plugin-clear";
 
-const progressOptions = {clearLine: false};
-
-const babelOptionsES2015 = {
+const babelOptionsES5 = {
     runtimeHelpers:  false,
     externalHelpers: false,
     babelrc:         false,
@@ -38,72 +35,66 @@ const babelOptionsES2018 = {
     ]
 };
 
-const terserOptions = {sourcemap: true};
-
 // https://rollupjs.org/guide/en#big-list-of-options
 export default [
     // ES2015 Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/observer.min.js',
-            format:    'umd',
-            name:      'ObjectObserver',
-            compact:   true,
+        input: './src/index.js',
+        output: {
+            file: './dist/observer.min.js',
+            format: 'umd',
+            name: 'ObjectObserver',
+            compact: true,
             sourcemap: true,
         },
         plugins: [
             resolve(),
             clear({targets: ['./dist']}),
-            progress(progressOptions),
-            babel(babelOptionsES2015),
-            terser(terserOptions),
+            babel(babelOptionsES5),
+            terser(),
         ]
     },
     // ES2015 None-Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/observer.js',
-            format:    'umd',
-            name:      'ObjectObserver',
-            compact:   false,
+        input: './src/index.js',
+        output: {
+            file: './dist/observer.js',
+            format: 'umd',
+            name: 'ObjectObserver',
+            compact: false,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress(progressOptions),
-            babel(babelOptionsES2015),
+            babel(babelOptionsES5),
         ]
     },
     // ES Modules Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/observer.esm.min.js',
-            format:    'esm',
-            compact:   true,
+        input: './src/index.js',
+        output: {
+            file: './dist/observer.esm.min.js',
+            format: 'esm',
+            compact: true,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress(progressOptions),
             babel(babelOptionsES2018),
-            terser(terserOptions),
+            terser(),
         ]
     },
     // ES Modules None-Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/observer.esm.js',
-            format:    'esm',
-            compact:   false,
+        input: './src/index.js',
+        output: {
+            file: './dist/observer.esm.js',
+            format: 'esm',
+            compact: false,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress(progressOptions),
             babel(babelOptionsES2018),
         ]
     },
